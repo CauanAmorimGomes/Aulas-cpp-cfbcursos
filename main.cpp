8b7dcc3928aa12e51367074ce0e5326d19e6859b
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-03-25 10:04:58 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    i=0; valor inicial        
+    while(i<5){  condição
+        printf("i = %d \n",i);
+        i++; condição
+    }
+
+    return 0;
+    foi executado 5 vezes
+    gerou 5 vezes a condição falsa
+}
